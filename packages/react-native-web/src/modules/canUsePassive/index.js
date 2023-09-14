a92d847574bb1302b50ae0abc80cbfa412c7ff24
Tienifr <christianwen18@gmail.com>
2023-09-14 17:07:34 +0700

    fix lint

--- packages/react-native-web/src/modules/canUsePassive/index.js
+++ packages/react-native-web/src/modules/canUsePassive/index.js
@@ -2,12 +2,14 @@ export default function canUsePassive() {
   let supportsPassive = false;
   try {
     const opts = Object.defineProperty({}, 'passive', {
-      get: function () {
+      // eslint-disable-next-line getter-return
+      get() {
         supportsPassive = true;
       }
     });
     window.addEventListener('testPassive', null, opts);
     window.removeEventListener('testPassive', null, opts);
+    // eslint-disable-next-line no-empty
   } catch (e) {}
   return supportsPassive;
 }
